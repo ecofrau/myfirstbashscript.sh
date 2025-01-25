00c71786166e5831dc31558b529d26916d9bb363
Anastasiia Myrka <myrkaa638@gmail.com>
2025-01-25 12:50:52 +0100

    Create myfirstbashscript.sh

---
+++ myfirstbashscript.sh
@@ -0,0 +1,10 @@
+test@MBP-Usertest ~ % touch ~/myfirstbashscript.sh
+test@MBP-Usertest ~ % chmod +x ~/myfirstbashscript.sh 
+test@MBP-Usertest ~ % nano ~/myfirstbashscript.sh
+test@MBP-Usertest ~ % ~/myfirstbashscript.sh
+Sat Jan 25 12:47:47 CET 2025
+Hello Anastasiia!
+Current directory: /Users/test
+Number of processes: 389
+test@MBP-Usertest ~ % 
+
